ff15d29cded5377dad3d4470f2f36ad31b30210f
karthik <119947493+karthik76765@users.noreply.github.com>
2022-12-09 19:17:33 +0530

    Add files via upload

---
+++ if.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	if 8%5 == 0 {
+		fmt.Println("8 is divided by 2")
+	} else {
+		fmt.Println("8 is not divided by 2")
+	}
+	if 6%3 == 0 {
+		fmt.Println("6 is divided by 3 is ture")
+	}
+}
